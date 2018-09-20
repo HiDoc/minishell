0203ff9ecefac3068b13858e096194918b5d584e
jyildiz <jyildiz-@student.42.fr>
2018-09-20 16:40:06 +0200

    modif libft

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: jyildiz- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 16:04:35 by jyildiz-          #+#    #+#             */
-/*   Updated: 2018/09/20 16:08:15 by jyildiz-         ###   ########.fr       */
+/*   Updated: 2018/09/20 16:39:51 by jyildiz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	collect_cmd(t_env *env, t_cmd *cmd)
 {
-
+	get_next_line(1, cmd->line) == -1 ? print_err(env, cmd) : 0;
+	parse(env, cmd);
 }
+
+void	parse(t_env *env, t_cmd *cmd)
+{
+
