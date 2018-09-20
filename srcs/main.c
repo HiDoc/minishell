72525c6c89669ccae1d6b859cc981274898484a1
HiDoc <fmadura@student.42.fr>
2018-09-20 18:20:41 +0200

    unit tests

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,22 @@
 /*   By: fmadura <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 15:04:23 by fmadura           #+#    #+#             */
-/*   Updated: 2018/09/20 15:56:35 by fmadura          ###   ########.fr       */
+/*   Updated: 2018/09/20 18:14:44 by fmadura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ms_prototype.h"
 
 extern char **environ;
-
+void	set_environ(t_env *env, char *key, char *value);
+void	unset_environ(t_env *env, char *key);
 int main(void)
 {
 	t_env	env;
 
 	init_env(&env);
+	set_environ(&env, "a", "a");	
+	//unset_environ(&env, "MAKELEVEL");	
 	print_environ(&env);
 	return (0);
 }
